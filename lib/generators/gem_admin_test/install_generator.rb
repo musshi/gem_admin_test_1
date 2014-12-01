dd2648a130812c27a5b8934b61c926165e0516ed
Musshi <musshi@rubify.com>
2014-12-01 15:51:31 +0700

    update code

--- lib/generators/gem_admin_test/install_generator.rb
+++ lib/generators/gem_admin_test/install_generator.rb
@@ -8,30 +8,29 @@ module GemAdminTest
 
       desc "Creates a AntsAdmin initializer and copy locale files to your application."
       class_option :orm
-
-      def copy_initializer
-        # template "ants_admin.rb", "config/initializers/ants_admin.rb"
-      end
-      
-      def copy_helper
-        template "application_helper.rb", "app/helps/application_helper.rb"
-      end
-      
-      def copy_view
-        template "dashboard.html.erb", "app/views/gem_admin_test/dashboard.html.erb"
-        
-      end
       
       def copy_menu_config_file
         template "config.yml", "config/config.yml"
       end
-
-      def inject_javascript
-        append_to_file 'app/assets/javascripts/application.js' do
-          out = "\n"
-          out << "//= require gem_admin_test/app"
-        end
+      
+      def remove_application_helper
+        remove_file 'app/helpers/application_helper.rb'
       end
+      
+      def copy_application_helper
+        template "application_helper.rb", "app/helpers/application_helper.rb"
+      end
+      
+      def copy_view
+        template "dashboard.html.erb", "app/views/gem_admin_test/dashboard.html.erb"       
+      end    
+      
+      # def inject_javascript
+        # append_to_file 'app/assets/javascripts/application.js' do
+          # out = "\n"
+          # out << "//= require gem_admin_test/app"
+        # end
+      # end
 
       def rails_4?
         Rails::VERSION::MAJOR == 4
