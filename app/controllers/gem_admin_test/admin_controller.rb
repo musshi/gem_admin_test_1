27ef2b1e5ed5f7b008c08e750e88381df206fb2c
Musshi <musshi@rubify.com>
2015-02-05 16:06:12 +0700

    update action

--- app/controllers/gem_admin_test/admin_controller.rb
+++ app/controllers/gem_admin_test/admin_controller.rb
@@ -12,16 +12,24 @@ class GemAdminTest::AdminController < GemAdminTestController
         @model_class = @model_string.classify.constantize
       rescue
         raise ActionController::RoutingError.new("Can not find model  #{@model_string.classify}")
-      end
-      
-      # p @model_string
-      # p @model_class
-      if url_s.count == 1
+      end  
+      if url_s.count == 1 # in this case http://localhost:3000/admin/products
         p "1"
         return index if request.get?
         return create if request.post?
-      else
-        
+      elsif url_s.count == 2 #in this case http://localhost:3000/admin/products/new
+        respond_to do |format|
+          format.json {
+            return json if request.get? and urls[1] == 'all'
+          }
+          format.html {
+            return new if request.get? and urls[1] == 'new'
+            return update(urls[1]) if request.patch?
+            return delete(urls[1]) if request.delete?
+            return add if request.get? and urls[1] == 'add'
+          }
+      elsif url_s.count == 3 #in this case http://localhost:3000/admin
+
       end
     rescue => e
       raise e 
@@ -36,6 +44,18 @@ class GemAdminTest::AdminController < GemAdminTestController
   def create
     params[:action] = "create"
     render template: "/gem_admin_test/form/create"
+  end
+  
+  def new
+  end
+  
+  def update
+  end
+  
+  def delete
+  end
+  
+  def add
   end
 
 end
